e402f10282c46e33df3375bd7c86eca94a45c0d8
Hamzah <abdillahhamzahali@gmail.com>
2025-02-09 11:23:05 +0700

    add : cors

--- src/application/web.ts
+++ src/application/web.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { publicRouter } from "../route/public-api";
 import { ErrorMiddleware } from "../middleware/error-middleware";
 import { apiRouter } from "../route/api";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(publicRouter);
 app.use(apiRouter);
